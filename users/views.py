866327e3498cb8b80467760929ac54f5a293b6be
Your Name <you@example.com>
2026-05-17 15:40:44 +0200

    profile

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 def register(request):
     if request.method == "POST":
@@ -18,5 +19,31 @@ def register(request):
 @login_required
 def profile(request):
     return render(request, 'profile.html')
+
+
+
+from .models import Profile
+from django.shortcuts import render, redirect
+
+def edit_profile(request):
+
+    profile, created = Profile.objects.get_or_create(
+        user=request.user
+    )
+
+    if request.method == "POST":
+
+        request.user.first_name = request.POST.get('first_name')
+        request.user.email = request.POST.get('email')
+
+        if 'avatar' in request.FILES:
+            profile.avatar = request.FILES['avatar']
+
+        request.user.save()
+        profile.save()
+
+        return redirect('profile')
+
+    return render(request, 'edit_profile.html')
 
 
